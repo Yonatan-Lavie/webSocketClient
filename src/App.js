e8f1829332e6e18ad650dfc83a369e53070a6a03
Yonatan Lavie <yonatan.lavie89@gmail.com>
2021-10-23 14:46:02 +0300

    react chat client using socket.io client using materialUI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,72 @@
-import React, { useEffect } from 'react';
-import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import TextField from "@material-ui/core/TextField"
+import React, { useEffect, useRef, useState } from "react"
+import io from "socket.io-client"
+import "./App.css"
 
-const client = new W3CWebSocket('ws://127.0.0.1:8000');
+function App() {
+	const [ state, setState ] = useState({ message: "", name: "" })
+	const [ chat, setChat ] = useState([])
 
-const App = () => {
-  useEffect(() => {
-    client.onopen = () => {
-      console.log('webSocket client Connected');
-    };
-    return () => {};
-  }, []);
+	const socketRef = useRef()
 
-  return <div>Testing this app!</div>;
-};
+	useEffect(
+		() => {
+			socketRef.current = io.connect("http://localhost:4000")
+			socketRef.current.on("message", ({ name, message }) => {
+				setChat([ ...chat, { name, message } ])
+			})
+			return () => socketRef.current.disconnect()
+		},
+		[ chat ]
+	)
 
-export default App;
+	const onTextChange = (e) => {
+		setState({ ...state, [e.target.name]: e.target.value })
+	}
+
+	const onMessageSubmit = (e) => {
+		const { name, message } = state
+		socketRef.current.emit("message", { name, message })
+		e.preventDefault()
+		setState({ message: "", name })
+	}
+
+	const renderChat = () => {
+		return chat.map(({ name, message }, index) => (
+			<div key={index}>
+				<h3>
+					{name}: <span>{message}</span>
+				</h3>
+			</div>
+		))
+	}
+
+	return (
+		<div className="card">
+			<form onSubmit={onMessageSubmit}>
+				<h1>Messenger</h1>
+				<div className="name-field">
+					<TextField name="name" onChange={(e) => onTextChange(e)} value={state.name} label="Name" />
+				</div>
+				<div>
+					<TextField
+						name="message"
+						onChange={(e) => onTextChange(e)}
+						value={state.message}
+						id="outlined-multiline-static"
+						variant="outlined"
+						label="Message"
+					/>
+				</div>
+				<button>Send Message</button>
+			</form>
+			<div className="render-chat">
+				<h1>Chat Log</h1>
+				{renderChat()}
+			</div>
+		</div>
+	)
+}
+
+export default App
+
